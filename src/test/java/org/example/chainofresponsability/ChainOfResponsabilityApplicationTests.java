86b6ee14b9584f66229ee65439946bd47a559302
Eduardo Lelis <eduardo.lelis.co@gmail.com>
2024-07-29 16:34:57 -0300

    init

---
+++ src/test/java/org/example/chainofresponsability/ChainOfResponsabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.chainofresponsability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChainOfResponsabilityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
